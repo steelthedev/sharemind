8219483d64bbf9a58e8ef6c9ea98ef60debfd3d2
Akinwumi iyanuoluwa Ayomiposi <65210097+steelthedev@users.noreply.github.com>
2021-12-25 06:24:07 +0100

    Update settings.py

--- experience/settings.py
+++ experience/settings.py
@@ -132,7 +132,7 @@ EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER='akinwumikaliyanu@gmail.com'
-EMAIL_HOST_PASSWORD='aiuaqgutrspxtvro'
+EMAIL_HOST_PASSWORD=''
 
 
 
